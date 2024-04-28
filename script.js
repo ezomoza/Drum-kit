aee1acca15b82c84982f91c15026c6f63eb19634
Emanuel Jose Zomoza <ema_z18@hotmail.com>
2024-04-28 00:38:55 -0300

    terminado

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return // stop function
+  audio.currentTime = 0; // rewind
+  audio.play()
+  key.classList.add('playing');
+  
+});
+
+function removeTransition(e) {
+if(e.propertyName !== 'transform') return; //skip if not transform
+  this.classList.remove('playing')
+}
+
+const key = document.querySelectorAll('.key')
+key.forEach(key => key.addEventListener('transitionend', removeTransition))
